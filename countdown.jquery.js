a1c658884e0144a09d791a63fab11058a468a759
Andros Romo <andros@pixblob.com>
2012-09-03 13:53:40 -0500

    Javascript terminado.
    Funciones básicas del contador funcionando en días, horas, minutos y
    segundos.

--- countdown.jquery.js
+++ countdown.jquery.js
@@ -1,31 +1,17 @@
-$(document).ready(function() {
-  
-  countdown(357,23,1,15);
-  
-  
-  
-});
-
 var global_Days = '';
 var global_Hour = '';
 var global_Mins = '';
 var global_Secs = '';
+var global_die  = false;
 
 function countdown(days,hours,minutes,seconds){
-    
-    var Boxs = '<input id="inputDays" type="hidden" value="'+days+'" />';
-        Boxs+= '<input id="inputHours" type="hidden" value="'+hours+'" />';
-        Boxs+= '<input id="inputMins" type="hidden" value="'+minutes+'" />';
-        Boxs+= '<input id="inputSecs" type="hidden" value="'+seconds+'" />';
     
     global_Days = days;
     global_Hour = hours;
     global_Mins = minutes;
     global_Secs = seconds;
     
-    $("#timeNext").append(Boxs);
-    
-    var Days = disjoin( zeroFill( String(days) , 2 ) );    
+    var Days = disjoin( zeroFill( String(days) , 3 ) );    
     $("#nextDays").html( Days.join('') );
 
     var Hours = disjoin( zeroFill( String(hours) , 2 ) );    
@@ -38,7 +24,7 @@ function countdown(days,hours,minutes,seconds){
     $("#nextSeconds").html( Seconds.join('') );
     
     
-    var sTseconds = setInterval('countSeconds()',1000);
+    sTseconds = setInterval('countSeconds()',1000);
     
 }
 
@@ -62,22 +48,44 @@ function disjoin(str){
 
 //clearInterval(sTseconds);
 
-function countSeconds(){
-    
-    var i = global_Secs;
-    
-    if( i < 1){
-        minusMinutes();
-        e = 59;    
-    } else {    
-        e = i-1;
+function endCountDown(){
+
+    if( global_Days < 1 && global_Hour < 1 && global_Mins < 1 && global_Secs < 1){
+
+        global_die = true;
+
+        clearInterval(sTseconds);
+
     }
+
+}
+
+function countSeconds(){
+
+    if( !global_die == true ){
     
-    global_Secs = e;
-    
-    var Seconds = disjoin( zeroFill( String(e) , 2 ) );    
-    $("#nextSeconds").html( Seconds.join('') );
-    
+        var i = global_Secs;
+        
+        if( i < 1){
+            minusMinutes();
+            e = 59;    
+        } else {    
+            e = i-1;
+        }
+        
+        global_Secs = e;
+        
+        var Seconds = disjoin( zeroFill( String(e) , 2 ) );    
+        $("#nextSeconds").html( Seconds.join('') );
+        
+        endCountDown();
+
+    } else {
+
+        alert("Sigo jodiendo...");
+
+    }
+
 }
 
 function minusMinutes(){
@@ -104,8 +112,8 @@ function minusHours(){
     var i = global_Hour;
     
     if( i < 1){
-        minusHours();
-        e = 59;    
+        minusDays();
+        e = 23;    
     } else {    
         e = i-1;
     }
@@ -117,29 +125,29 @@ function minusHours(){
     
 }
 
+function minusDays(){
+    
+    var i = global_Days;
+    
+    if( i < 1){
+        e = 0;    
+    } else {    
+        e = i-1;
+    }
+    
+    global_Days = e;
+    
+    var Days = disjoin( zeroFill( String(e) , 3 ) );    
+    $("#nextDays").html( Days.join('') );
+    
+}
+
 
 
 function zeroFill( number, width ){
   width -= number.toString().length;  if ( width > 0 ){   return new Array( width + (/\./.test( number ) ? 2 : 1) ).join( '0' ) + number;   } return number + ""; 
 }
 
-function explode (delimiter, string, limit) {
-    var emptyArray = { 0: '' }; if (arguments.length < 2 || typeof arguments[0] == 'undefined' || typeof arguments[1] == 'undefined') { return null;  }  if (delimiter === '' || delimiter === false || delimiter === null) { return false; }  if (typeof delimiter == 'function' || typeof delimiter == 'object' || typeof string == 'function' || typeof string == 'object') { return emptyArray; }   if (delimiter === true) { delimiter = '1'; }  if (!limit) { return string.toString().split(delimiter.toString()); } else {  var splitted = string.toString().split(delimiter.toString()); var partA = splitted.splice(0, limit - 1);      var partB = splitted.join(delimiter.toString());        partA.push(partB);        return partA;    }
-}
-
-function str_replace (search, replace, subject, count) {
-    var i = 0, j = 0, temp = '', repl = '', sl = 0, fl = 0, f = [].concat(search), r = [].concat(replace), s = subject, ra = Object.prototype.toString.call(r) === '[object Array]', sa = Object.prototype.toString.call(s) === '[object Array]';    s = [].concat(s);    if (count) {        this.window[count] = 0;    }     for (i = 0, sl = s.length; i < sl; i++) {        if (s[i] === '') {            continue;        }        for (j = 0, fl = f.length; j < fl; j++) {            temp = s[i] + '';            repl = ra ? (r[j] !== undefined ? r[j] : '') : r[0];            s[i] = (temp).split(f[j]).join(repl);            if (count && s[i] !== temp) {                this.window[count] += (temp.length - s[i].length) / f[j].length;            }        }    }    return sa ? s : s[0];
-}
-
-
 function substr (str, start, len) {
     var i = 0,        allBMP = true,        es = 0,        el = 0,        se = 0,        ret = '';    str += '';    var end = str.length;      this.php_js = this.php_js || {};    this.php_js.ini = this.php_js.ini || {};     switch ((this.php_js.ini['unicode.semantics'] && this.php_js.ini['unicode.semantics'].local_value.toLowerCase())) {    case 'on':         for (i = 0; i < str.length; i++) {            if (/[\uD800-\uDBFF]/.test(str.charAt(i)) && /[\uDC00-\uDFFF]/.test(str.charAt(i + 1))) {                allBMP = false;                break;            }        }         if (!allBMP) {            if (start < 0) {                for (i = end - 1, es = (start += end); i >= es; i--) {                    if (/[\uDC00-\uDFFF]/.test(str.charAt(i)) && /[\uD800-\uDBFF]/.test(str.charAt(i - 1))) {                        start--;                        es--;                    }                }            } else {                var surrogatePairs = /[\uD800-\uDBFF][\uDC00-\uDFFF]/g;                while ((surrogatePairs.exec(str)) != null) {                    var li = surrogatePairs.lastIndex;                    if (li - 2 < start) {                        start++;                    } else {                        break;                    }                }            }             if (start >= end || start < 0) {                return false;            }            if (len < 0) {                for (i = end - 1, el = (end += len); i >= el; i--) {                    if (/[\uDC00-\uDFFF]/.test(str.charAt(i)) && /[\uD800-\uDBFF]/.test(str.charAt(i - 1))) {                        end--;                        el--;                    }                }                if (start > end) {                    return false;                }                return str.slice(start, end);            } else {                se = start + len;                for (i = start; i < se; i++) {                    ret += str.charAt(i);                    if (/[\uD800-\uDBFF]/.test(str.charAt(i)) && /[\uDC00-\uDFFF]/.test(str.charAt(i + 1))) {                        se++;                     }                }                return ret;            }            break;        }     case 'off':    default:        if (start < 0) {            start += end;        }        end = typeof len === 'undefined' ? end : (len < 0 ? len + end : len + start);         return start >= str.length || start < 0 || start > end ? !1 : str.slice(start, end);    }    return undefined; 
-}
-
-function stristr (haystack, needle, bool) {
-    var pos = 0;     haystack += '';    pos = haystack.toLowerCase().indexOf((needle + '').toLowerCase());    if (pos == -1) {        return false;    } else {        if (bool) {            return haystack.substr(0, pos);        } else {            return haystack.slice(pos);        }    }
-}
-
-function getUrlVars() {
-	var vars = {};	var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m,key,value) {		vars[key] = value;	});	return vars;
 }
